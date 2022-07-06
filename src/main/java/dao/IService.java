14da1714c76c771db11f53aed076fdcb42749043
Chu Tinh <tinhteng@gmail>
2022-07-06 17:26:48 +0700

    up1

---
+++ src/main/java/dao/IService.java
@@ -0,0 +1,13 @@
+package dao;
+
+import Models.Sanpham;
+
+import java.util.List;
+
+public interface IService<E> {
+    public List<E> getAll();
+    public boolean create(E e);
+    public boolean edit(int id,E e);
+    public boolean delete(int id);
+    public E findById(int id);
+}
